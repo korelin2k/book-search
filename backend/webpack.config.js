219915b630ee3b2931b777cc2f82c618912c9e2f
Chris Figgins <figginsc@gmail.com>
2019-02-18 15:49:34 -0500

    Updated package versions for everything... lets see what works in Travis & Heroku. Should be entertaining

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -4,33 +4,38 @@ const NodemonPlugin = require('nodemon-webpack-plugin');
 
 const nodeModules = {};
 fs.readdirSync('node_modules')
-  .filter(function(x) {
-    return ['.bin'].indexOf(x) === -1;
-  })
-  .forEach(function(mod) {
-    nodeModules[mod] = 'commonjs ' + mod;
-  });
+    .filter(function (x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function (mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
 
 module.exports = {
-  entry: './server/server.ts',
-  output: {
-    path: path.join(__dirname, '/build'),
-    filename: 'server.js'
-  },
-  resolve: {
-    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader'
-      }
+    mode: "development",
+    entry: './server/server.ts',
+    output: {
+        path: path.join(__dirname, '/build'),
+        filename: 'server.js'
+    },
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)?$/,
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    target: 'node',
+    externals: nodeModules,
+    plugins: [
+        new NodemonPlugin()
     ]
-  },
-  target: 'node',
-  externals: nodeModules,
-  plugins: [
-    new NodemonPlugin()
-  ]
 };
